f2515c2d87bdcbd7464208afc5db7f495526a72c
maingo2005 <k023g1006@g.neec.ac.jp>
2024-06-21 12:38:33 +0900

    PG3_04_03_課題完了

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
+class Animal {
+public:
+	virtual void Voice() {
+		printf("“®•¨\n");
+	}
+};
+
+class Dog :public Animal {
+public:
+	void Voice() {
+		printf("ƒƒ“\n");
+	}
+};
+
+class Cat :public Animal {
+public:
+	void Voice() {
+		printf("ƒjƒƒƒ“\n");
+	}
+};
+
 int main() {
+
+	Animal* animal[2] = { new Dog,new Cat };
+
+	animal[0]->Voice();
+	animal[1]->Voice();
+
+	delete animal[0];
+	delete animal[1];
 
 	return 0;
 }
