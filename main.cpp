9a45e12f119a6a419de1173f4a9b2ea683f20791
maingo2005 <k023g1006@g.neec.ac.jp>
2024-07-23 16:46:49 +0900

    08_01_課題完了

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
-#include <stdio.h>
+#include<stdio.h>
+#include"string.h"
+#include <string>
+#include <chrono>
+#include <iostream>
+template<typename T>
 
-int main() {
+T Move(T&& right) {
+	return right;
+}
 
-	return 0;
+int main(){
+
+	std::chrono::system_clock::time_point stratTime, endTime;
+	stratTime = std::chrono::system_clock::now();
+	std::string A(100000, 'a');
+	printf("100,000文字を移動とコピーで比較しました。\n");
+
+	std::string copyConstructed(A);
+	endTime = std::chrono::system_clock::now();
+
+	auto millisec = std::chrono::duration_cast<std::chrono::microseconds>(endTime - stratTime).count();
+	std::cout << " コピー:" << millisec << "us\n" << std::endl;
+
+	stratTime = std::chrono::system_clock::now();
+
+	std::string moveConstructed(std::move(A));
+	endTime = std::chrono::system_clock::now();
+
+	auto m = std::chrono::duration_cast<std::chrono::microseconds>(endTime - stratTime).count();
+	std::cout << "移動:" << m << "us\n" << std::endl;
 }
