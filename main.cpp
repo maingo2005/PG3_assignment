e2f831d9ea67861baac51b096edf6a645e5acd2f
maingo2005 <k023g1006@g.neec.ac.jp>
2024-07-23 16:35:24 +0900

    07_02_課題完了

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <thread>
+#include<Windows.h>
+#include <functional>
+#include <list>
+#include<vector>
+#include <iostream>
+using namespace std;
+
+void Output(string a){
+	cout << a << "\n";
+}
 
 int main() {
+	string iti = "threas1";
+	string ni = "threas2";
+	string san = "threas3";
+
+	std::thread th1(Output, iti);
+	th1.join();
+
+	std::thread th2(Output, ni);
+	th2.join();
+
+	std::thread th3(Output, san);
+	th3.join();
 
 	return 0;
 }
